30e3973fd85a74657582176b966c4387d5e67b03
Serena Wang <serenalwang@gmail.com>
2015-12-04 07:29:50 -0500

    added training data to repo

--- extract_features.sh
+++ extract_features.sh
@@ -4,9 +4,7 @@
 # Extracts all features from programs in SERENAPROGRAMSDIR
 ###
 
-SERENAPROGRAMSDIR=asc-0.1.4/serena-programs-processed
-# Number of serena programs to benchmark with ASC
-NPROGRAMS=5
+SERENAPROGRAMSDIR=asc-0.1.4/kernels
 
 # Clear program text features from previous runs
 echo "program_number,num_lines,num_words,num_chars" > program_text_features.csv
@@ -17,22 +15,36 @@ echo "program_number,num_functions,num_function_calls,total_time,highest_percent
 echo "program_number,Ir,Dr,I1mr,d1mr,D1mw,ILmr,DLmr,DLmw,I1missrate,D1missread,D1misswrite,LLmissrate" > program_callgrind_features.csv
 
 # Extract features for each program
-for i in `seq 1 $NPROGRAMS`;
+KERNELS=(collatz factor 3sum warshall)
+
+declare -A kernel_arg_map
+kernel_arg_map["collatz"]="100000"
+kernel_arg_map["warshall"]="1 1500"
+kernel_arg_map["factor"]="9000037038035596873"
+kernel_arg_map["loop"]="9223372000000000000 100000000000"
+kernel_arg_map["3sum"]="$RANDOM 5000 1750000000"
+
+for kernel in "${KERNELS[@]}";
 do
+    echo extracting features for $kernel
     # Extract text features  
-    python program_text_features.py $SERENAPROGRAMSDIR/$i.c
-    
+    python program_text_features.py $SERENAPROGRAMSDIR/$kernel.c
+
+    echo extracting gprof features
     # Extract gprof features
-    gcc -pg $SERENAPROGRAMSDIR/$i.c -o $i
-    ./$i
-    gprof $i gmon.out -b | python program_gprof_features.py $i
+    args="${kernel_arg_map[${kernel}]}"
+    gcc -pg $SERENAPROGRAMSDIR/$kernel.c -o $kernel
+    echo running $kernel $args
+    ./$kernel $args
+    gprof $kernel gmon.out -b | python program_gprof_features.py $kernel
     rm -f gmon.out
-    
+
+    echo extracting callgrind features
     # Extract callgrind features
-    valgrind --tool=callgrind --simulate-cache=yes ./$i 3>&2 2>&1 1>&3 | python program_callgrind_features.py $i
+    valgrind --tool=callgrind --simulate-cache=yes ./$kernel $args 3>&2 2>&1 1>&3 | python program_callgrind_features.py $kernel
     rm -f callgrind.out*
     
     # Clean up unnecessary files
-    rm -f $i
+    rm -f $kernel
     
 done
