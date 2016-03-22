3f0d90455a8f9fbc0f894ef610d3b6e4c903d0ba
Serena Wang <serenalwang@gmail.com>
2016-03-22 13:58:19 -0400

    fixed runprogip.sh to vary the number of rounds trained on each input

--- asc-0.1.4/runprogip.sh
+++ asc-0.1.4/runprogip.sh
@@ -20,32 +20,40 @@ NRUNS=$4
 # Seed the bash random number generator with the current process id
 RANDOM=$$
 # Max number of rounds per run
-MAXROUNDS="50"
+MAXROUNDS="10"
 OUTDIR=ascfeatures
-# Store results in output csv file
-OUTFILE=$PROGNAME-$IP-asc.csv
-
 mkdir -p $OUTDIR
-# Clear output file
-rm -f $OUTDIR/$OUTFILE
-# Clear previous .net file
-rm -f $PROGNAME.net
 
-echo RUNNING $PROGNAME FOR IP $IP
-# Local variable - number of runs that have already been completed
-INPUTSDONE="0"
-while [ $INPUTSDONE -lt $NINPUTS ];
+# Local variable - number of rounds for the current epoch
+EPOCHRUNS="0"
+while [ $EPOCHRUNS -lt $NRUNS ];
 do
-    INPUTSDONE=$[$INPUTSDONE+1]
-    echo starting input $INPUTSDONE
-    INPUT=$RANDOM
-    echo random input $INPUT
-    RUNSDONE="0"
-    while [ $RUNSDONE -lt $NRUNS ];
+    EPOCHRUNS=$[$EPOCHRUNS+1]
+    # Store results in output csv file
+    OUTFILE=$PROGNAME-$IP-$EPOCHRUNS-asc.csv
+
+    # Clear output file
+    rm -f $OUTDIR/$OUTFILE
+    # Clear previous .net file
+    rm -f $PROGNAME.net
+
+    echo RUNNING $PROGNAME FOR IP $IP AT $EPOCHRUNS RUNS PER INPUT
+
+    # Local variable - number of runs that have already been completed
+    INPUTSDONE="0"
+    while [ $INPUTSDONE -lt $NINPUTS ];
     do
-        RUNSDONE=$[$RUNSDONE+1]
-	echo starting run $RUNSDONE for input $INPUT
-	./asc -a $IP -n $MAXROUNDS $PROGNAME.net $PROGNAME $INPUT | python program_asc_features.py $PROGNAME $IP $INPUT $RUNSDONE $OUTDIR/$OUTFILE
-    done
-    echo $INPUTSDONE inputs completed
-done 
+	INPUTSDONE=$[$INPUTSDONE+1]
+	echo starting input $INPUTSDONE
+	INPUT=$RANDOM
+	echo random input $INPUT
+	RUNSDONE="0"
+	while [ $RUNSDONE -lt $EPOCHRUNS ];
+	do
+            RUNSDONE=$[$RUNSDONE+1]
+	    echo starting run $RUNSDONE for input $INPUT
+	    ./asc -a $IP -n $MAXROUNDS $PROGNAME.net $PROGNAME $INPUT | python program_asc_features.py $PROGNAME $IP $INPUT $RUNSDONE $OUTDIR/$OUTFILE
+	done
+	echo $INPUTSDONE inputs completed
+    done 
+done
