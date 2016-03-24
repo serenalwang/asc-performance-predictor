541a4702cccbb73ff6a2a13af7598e7e8705f087
Serena Wang <serenalwang@gmail.com>
2016-03-24 11:27:09 -0400

    got all callgrind features

--- asc-0.1.4/callgrind_ir_features.sh
+++ asc-0.1.4/callgrind_ir_features.sh
@@ -7,23 +7,20 @@
 # Also assumes that callgrind infiles already exist in the format <program name>-callgrind.out
 # To generate callgrind infile:
 # $ valgrind --tool=callgrind --dump-instr=yes ./<program name> <random seed>
-# $ callgrind-annotate --auto=yes callgrind.out.pid
+# $ callgrind_annotate --auto=yes callgrind.out.pid > <program name>-callgrind.out
 #
-# USAGE: bash callgrind_ir_features.sh 
+# USAGE: bash callgrind_ir_features.sh <program name>
 ###
 
-SERENAPROGRAMS=(collatz_random random)
+program=$1
 
 OUTFILE=callgrind_ir_features.csv
 
 # clear outfile
-rm -f $OUTFILE
+# rm -f $OUTFILE
 
-for program in $SERENAPROGRAMS
-do
     objdump --dwarf=decodedline $program > $program-dwarf.out
     objdump -d $program > $program-objdump.out
     python callgrind_ir_features.py $program-callgrind.out $program-dwarf.out $program-objdump.out $program $OUTFILE
     rm $program-objdump.out
     rm $program-dwarf.out
-done 
