69553e6b05e484f2bfee18a9ed46de719d34867b
serenalwang <serenalwang@gmail.com>
2016-03-24 10:50:08 -0400

    fixed objdump ip features

--- asc-0.1.4/objdump_ip_features.py
+++ asc-0.1.4/objdump_ip_features.py
@@ -27,16 +27,20 @@ or_insts = {"or", "xor", "and"}
 ret_insts = {"retq"}
 
 insts = [jmp_insts, call_insts, mov_insts, lea_insts, cmp_insts, inc_insts, mul_insts, add_insts, or_insts, push_insts]
-    
+
 #objdump_string = "\ncollatz_serena:     file format elf64-x86-64\n\n\nDisassembly of section .text:\n0000000000400144 <_start>:\n  400144:	48 31 ed             	xor    %rbp,%rbp\n  400147:	5f                   	pop    %rdi\n  400148:	48 89 e6             	mov    %rsp,%rsi\n  40014b:	48 83 e4 f0          	and    $0xfffffffffffffff0,%rsp\n\n\n0000000000400161 <step>:\n  400161:	55                   	push   %rbp\n  400162:	48 89 e5             	mov    %rsp,%rbp\n  40023e:	4c 89 e7             	mov    %r12,%rdi\n  400241:	e8 1b ff ff ff       	callq  400161 <step>\n  400246:	49 89 c4             	mov    %rax,%r12\n  400249:	49 83 fc 01          	cmp    $0x1,%r12\n  40024d:	77 ef                	ja     40023e <main+0x9c>\n"
 
-# Extracts all ip values from an entire objdump output. 
-def extract_all_ips(objdump_string):
+# Extracts all ip values from an entire objdump output.
+def extract_all_ips(objdump_infile):
+    inf = open(objdump_infile, 'r')
+    objdump_string = inf.read()
+
     # ips is a dictionary with key: ip, value: [list of features]
     ips = OrderedDict()
     jmp_targets = set()
     start_str = "<_start>:"
     ip_divider_str = "\n  "
+
     # Initialize start of listed ips
     ip_start = objdump_string.find(start_str)
     assert(ip_start != -1)
@@ -52,31 +56,31 @@ def extract_all_ips(objdump_string):
         ip_string = objdump_string[:line_end]
         ip_string_split = ip_string.split("\t")
         # print ip_string_split
-        inst_string = ip_string_split[2]
-        inst_string_split = inst_string.split()
-        inst = inst_string_split[0]
 
-        # Check for target of jmp instruction
-        if inst in jmp_insts:
-            jmp_target = inst_string_split[1]
-            jmp_targets.add(jmp_target)
-        
-        # Match instruction with feature index
-        ip_features = []
-        for inst_set in insts:
-            if inst in inst_set:
-                ip_features.append(1)
-            else:
-                ip_features.append(0)
-        ips[ip] = ip_features
+        if len(ip_string_split) > 2:
+            inst_string = ip_string_split[2]
+            inst_string_split = inst_string.split()
+            inst = inst_string_split[0]
+
+            # Check for target of jmp instruction
+            if inst in jmp_insts:
+                jmp_target = inst_string_split[1]
+                jmp_targets.add(jmp_target)
+
+            # Match instruction with feature index
+            ip_features = []
+            for inst_set in insts:
+                if inst in inst_set:
+                    ip_features.append(1)
+                else:
+                    ip_features.append(0)
+            ips[ip] = ip_features
         # Advance to the next ip_start
         ip_start = objdump_string.find(ip_divider_str)
     return ips, jmp_targets
 
 infile = sys.argv[1]
-inf = open(infile, 'r')
-objdump_string = inf.readline()
-ips_dict, jmp_target_set = extract_all_ips(objdump_string)
+ips_dict, jmp_target_set = extract_all_ips(infile)
 #print ips_dict
 #print jmp_target_set
 
